b8eb4398563d4897a5e57f428b1562191b84df96
Filippo Rosso <temiwp@giusi.org>
2011-07-14 22:15:40 +0200

    readded wp-config

---
+++ wp-content/themes/focus/js/focus-script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function() {
+
+jQuery('#upload_image_button').click(function() {
+ formfield = jQuery('#upload_image').attr('name');
+ tb_show('', 'media-upload.php?type=image&amp;TB_iframe=true');
+ return false;
+});
+
+window.send_to_editor = function(html) {
+ imgurl = jQuery('img',html).attr('src');
+ jQuery('#upload_image').val(imgurl);
+ jQuery('#logo_url').val(imgurl);
+ tb_remove();
+}
+
+});
